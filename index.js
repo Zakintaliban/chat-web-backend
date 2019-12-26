06dcda5fc9d82d62de6acb12164463616489536f
zakintaliban <nukmans23@gmail.com>
2019-12-26 22:51:09 +0700

    minor fixes

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("http");
 const cors = require("cors");
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
-
-const PORT = process.env.PORT || 5000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const router = require("./router");
 
@@ -22,6 +20,8 @@ io.on("connection", socket => {
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit("message", {
       user: "admin",
       text: `${user.name}, welcome to the room ${user.room}`
@@ -29,8 +29,6 @@ io.on("connection", socket => {
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", text: `${user.name}, has joined!` });
-
-    socket.join(user.room);
 
     io.to(user.room).emit("roomData", {
       room: user.room,
@@ -44,10 +42,6 @@ io.on("connection", socket => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
-    io.to(user.room).emit("roomData", {
-      room: user.room,
-      users: getUsersInRoom(user.room)
-    });
 
     callback();
   });
@@ -57,13 +51,17 @@ io.on("connection", socket => {
 
     if (user) {
       io.to(user.room).emit("message", {
-        user: "admin",
+        user: "Admin",
         text: `${user.name} has left.`
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
       });
     }
   });
 });
 
-app.use(router);
-
-server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
+);
